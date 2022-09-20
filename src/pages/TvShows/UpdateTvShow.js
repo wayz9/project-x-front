990c7553ef971c299cf82dc26915bd2be6fabd7a
Ilija Radivojevic <ilija.radivojevic237@gmail.com>
2022-09-20 19:32:51 +0200

    Fix accordion expansion

--- src/pages/TvShows/UpdateTvShow.js
+++ src/pages/TvShows/UpdateTvShow.js
@@ -1,19 +1,8 @@
 import { Link } from 'react-router-dom'
-import {
-  ArrowNarrowRight,
-  BrandAsana,
-  ChevronDown,
-  Components,
-  Crown,
-  Focus2,
-  Language,
-  Pencil
-} from 'tabler-icons-react'
-import { Collapse } from 'react-collapse'
-import { useState } from 'react'
+import { BrandAsana, Components, Crown, Focus2, Language } from 'tabler-icons-react'
+import SeasonAccordion from '../../components/SeasonAccordion'
 
 const UpdateTvShow = () => {
-  const [isOpened, setIsOpened] = useState(false)
   const episodes = [
     {
       id: 1,
@@ -119,61 +108,7 @@ const UpdateTvShow = () => {
             <label htmlFor="seasons">Seasons</label>
             <div className="mt-2.5 flex flex-col gap-y-5 md:mt-3">
               {['I', 'II', 'III', 'IV', 'V'].map((season, index) => (
-                <div key={index} className="rounded-lg border border-gray-200 bg-white">
-                  <div className="flex items-center justify-between">
-                    <div className="flex items-center">
-                      <div className="flex h-16 w-[4.5rem] items-center justify-center text-center text-xl text-gray-400">
-                        <span>{season}</span>
-                      </div>
-                      <div>
-                        <p className="text-base font-medium text-gray-800">season_{index + 1}.0</p>
-                        <p className="mt-1 text-sm text-gray-400">10 episodes</p>
-                      </div>
-                    </div>
-                    <button
-                      onClick={() => setIsOpened(!isOpened)}
-                      className="mr-5 rounded-md p-2 text-gray-600 hover:bg-gray-50 focus:text-gray-800">
-                      <ChevronDown
-                        className={isOpened ? 'rotate-180 transition-all' : 'transition-all'}
-                      />
-                    </button>
-                  </div>
-                  <Collapse isOpened={isOpened}>
-                    <div className="bg-dotted-pattern bg-top bg-repeat-x px-6 py-5">
-                      <ul className="flex flex-col gap-y-4">
-                        {episodes.map((episode) => (
-                          <li key={episode.id} className="flex items-center justify-between">
-                            <div className="flex items-center gap-x-5">
-                              <button className="text-gray-400 transition-colors hover:text-gray-800">
-                                <Pencil />
-                              </button>
-                              <p className="text-md leading-6 text-gray-800 line-clamp-1">
-                                {episode.title}
-                              </p>
-                            </div>
-                            <div className="whitespace-nowrap text-sm text-gray-400">
-                              {episode.watchtime}
-                            </div>
-                          </li>
-                        ))}
-                      </ul>
-                      <div className="mt-6 flex items-center justify-between md:mt-8">
-                        <Link
-                          to="/tvShows/1"
-                          className="flex items-center text-sm font-medium text-gray-800 focus:outline-none">
-                          <span>Manage</span>
-                          <span className="text-gray-400">
-                            <ArrowNarrowRight size={20} />
-                          </span>
-                        </Link>
-                        <span className="text-sm">
-                          <span className="text-gray-400">Watchtime:</span>
-                          <span className="pl-2 font-medium text-gray-600">6h 47min</span>
-                        </span>
-                      </div>
-                    </div>
-                  </Collapse>
-                </div>
+                <SeasonAccordion key={index} index={index} season={season} episodes={episodes} />
               ))}
             </div>
           </div>
